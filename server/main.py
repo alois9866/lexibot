3fc0ff05d09b27208be22653b3f5256e7c0ca81c
alois9866 <5354247+alois9866@users.noreply.github.com>
2021-03-04 01:15:31 +0300

    Add server DB model draft (#2)
    * Add server DB model draft
    
    * Fix dependencies

--- server/main.py
+++ server/main.py
@@ -1,4 +1,29 @@
 """This module is the main module of lexibot server."""
 
+import os
+import sys
+
+import psycopg2
+
+USER_VAR = 'DBUSER'
+PASSWORD_VAR = 'DBPASSWORD'
+HOST_VAR = 'DBHOST'
+
 if __name__ == '__main__':
-    print('Hello, World!')
+    user = os.environ.get(USER_VAR)
+    if user is None:
+        sys.exit(USER_VAR + ' is not set')
+
+    password = os.environ.get(PASSWORD_VAR)
+    if password is None:
+        sys.exit(PASSWORD_VAR + ' is not set')
+
+    host = os.environ.get(HOST_VAR)
+    if host is None:
+        sys.exit(HOST_VAR + ' is not set')
+
+    conn = psycopg2.connect(database="postgres", user=user, password=password, host=host, port="5432")
+
+    # TODO
+
+    conn.close()
