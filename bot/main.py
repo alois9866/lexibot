b4408f978f02120ae8a82faf2cee7167ded2f3db
alois9866 <alois9866@gmail.com>
2021-03-03 21:01:14 +0300

    Fix pylint

--- bot/main.py
+++ bot/main.py
@@ -1,2 +1,4 @@
+"""This module is the main module of lexibot client, the bot."""
+
 if __name__ == '__main__':
     print('Hello, World!')
--- server/main.py
+++ server/main.py
@@ -1,2 +1,4 @@
+"""This module is the main module of lexibot server."""
+
 if __name__ == '__main__':
     print('Hello, World!')
