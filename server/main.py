69aa49de47f73e649916c75ff653ba7bee122e07
alois9866 <5354247+alois9866@users.noreply.github.com>
2021-03-04 14:10:28 +0300

    Add basic server logic (#6)
    * Add basic server logic
    
    * Add bjoern dependencies to CI
    
    * Add sudo
    
    * Fix typo

--- server/main.py
+++ server/main.py
@@ -3,11 +3,17 @@
 import os
 import sys
 
+import bjoern
 import psycopg2
+
+import application
 
 USER_VAR = 'DBUSER'
 PASSWORD_VAR = 'DBPASSWORD'
-HOST_VAR = 'DBHOST'
+DB_HOST_VAR = 'DBHOST'
+
+HOST = '0.0.0.0'
+PORT = 80
 
 if __name__ == '__main__':
     user = os.environ.get(USER_VAR)
@@ -18,12 +24,14 @@ if __name__ == '__main__':
     if password is None:
         sys.exit(PASSWORD_VAR + ' is not set')
 
-    host = os.environ.get(HOST_VAR)
-    if host is None:
-        sys.exit(HOST_VAR + ' is not set')
+    db_host = os.environ.get(DB_HOST_VAR)
+    if db_host is None:
+        sys.exit(DB_HOST_VAR + ' is not set')
 
-    conn = psycopg2.connect(database="postgres", user=user, password=password, host=host, port="5432")
-
-    # TODO
-
-    conn.close()
+    conn = psycopg2.connect(database="postgres", user=user, password=password, host=db_host, port="5432")
+    try:
+        app = application.initialize_app(conn)
+        print(f'Starting server at {HOST}:{PORT}')
+        bjoern.run(app, HOST, PORT)
+    finally:
+        conn.close()
