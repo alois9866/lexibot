e9925f6d6d451c789bc3869e338e14c1e7969db4
Alex Khakhmovich <hahmovich@gmail.com>
2021-03-06 13:54:17 +0300

    Add demonstration example of Telegram bot

--- bot/main.py
+++ bot/main.py
@@ -1,4 +1,28 @@
 """This module is the main module of lexibot client, the bot."""
 
+import argparse
+import json
+
+from bot import telegram_bot
+
+
+def main():
+    parser = argparse.ArgumentParser(description="Main script to invoke "
+                                     "custom bots")
+    parser.add_argument("-bcp", "--bot_config_path", type=str,
+                        help="\t.json configuration file for the bot, example:"
+                        "\t\t`config.json`")
+    args = parser.parse_args()
+    with open(args.bot_config_path, "r") as rf:
+        config = json.load(rf)
+    bot_type = config["bot_type"]
+    if bot_type == "telegram":
+        assert "token" in config["telegram"], ("For `telegram` bot type you"
+                                               "have to provide Telegram token")
+        telegram_bot.run(config["telegram"]["token"])
+    else:
+        pass
+
+
 if __name__ == '__main__':
-    print('Hello, World!')
+    main()
