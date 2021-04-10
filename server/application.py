1d19c81c6733333afdcd9845d5c0a8fc0fda3c28
alois9866 <alois9866@gmail.com>
2021-04-10 19:13:40 +0300

    Rewrite request processing logic

--- server/application.py
+++ server/application.py
@@ -1,4 +1,5 @@
 """This package contains the server itself and handlers for it's endpoints."""
+import datetime
 import json
 
 import flask
@@ -7,19 +8,26 @@ import model
 
 _app = flask.Flask(__name__)
 _dbConnection = None
+_bot_token_hash = None
 
 
-def initialize_app(conn) -> flask.Flask:
-    """Returns Flask application initialized with DB connection"""
-    global _dbConnection
+def _initialized():
+    """Returns True if all parameters of the application were set."""
+    return _dbConnection and _bot_token_hash
+
+
+def initialize_app(conn, bot_token_hash) -> flask.Flask:
+    """Returns Flask application initialized with DB connection."""
+    global _dbConnection, _bot_token_hash
     _dbConnection = conn
+    _bot_token_hash = bot_token_hash
     return _app
 
 
 @_app.route('/i/<int:link_id>', methods=['GET'])
 def click(link_id: int):
-    """Handles short link click"""
-    if _dbConnection is None:
+    """Handles short-link click."""
+    if not _initialized():
         return 'Not started.', 500
 
     full_link = model.handle_click(_dbConnection, link_id)
@@ -28,26 +36,53 @@ def click(link_id: int):
 
 @_app.route('/create', methods=['POST'])
 def create():
-    """Creates new link. Takes JSON object as an input, which should contain chat id as 'chat_id', word as 'word' and bot token as 'token'."""
-    if _dbConnection is None:
-        return 'Not started.', 500
+    """
+    Creates new short-link.
 
-    if not flask.request.is_json():
+    Takes JSON object as an input, which should contain chat id as 'chat_id', word as 'word' and bot token hash as 'token_hash'.
+
+    Returns the ID of newly created link.
+    """
+    if not _initialized():
+        return 'Not started.', 500
+    if not flask.request.is_json:
         return 'Request not in JSON format', 400
 
-    data = json.loads(flask.request.get_json())[0]
-    token = data.get('token')
-    if token is None or len(token) == 0:
-        return 'No bot token', 401
+    data = flask.request.get_json()
+    if not data:
+        return 'No valid input provided', 401
 
-    if not model.bot_registered(_dbConnection, token):
-        return 'Incorrect bot token', 401
+    token_hash = data.get('token_hash')
+    if not token_hash:
+        return 'No bot token hash provided', 401
+    if token_hash != _bot_token_hash:
+        return 'Incorrect bot token hash', 401
 
-    full_link = model.create(_dbConnection, data.get('chat_id'), data.get('word'))
-    return flask.redirect(full_link)
+    return str(model.create(_dbConnection, data.get('chat_id'), data.get('word')))
+
+
+@_app.route('/top/<string:chat_id>', methods=['GET'])
+def get_top(chat_id: str):
+    """
+    Returns pairs of (word, number of clicks) for top 5 clicked words for chat with 'chat_id'.
+
+    Takes optional parameter 'date'. The result will be filtered for the time period, which includes date.
+    If 'date' parameter is not provided it is defaulted to current date in UTC.
+    """
+    if not chat_id:
+        return 'No chat ID provided', 401
+
+    date = flask.request.args.get('date')
+    if not date:
+        date = datetime.datetime.utcnow().date()
+    else:
+        date = datetime.datetime.strptime(date, '%Y-%m-%d').date()
+
+    pairs = model.get_words_with_clicks(_dbConnection, chat_id, date)
+    return json.dumps({'words': pairs})
 
 
 @_app.errorhandler(Exception)
 def all_exception_handler(error):
-    """Handles all exceptions"""
-    return f'Error: {error}.', 500
+    """Handles all exceptions."""
+    return f'Error: {error}', 500
--- server/model.py
+++ server/model.py
@@ -1,50 +1,46 @@
 """This module contains functions to work with link DB."""
 
 import datetime
-import uuid
 
-JISHO_URL = 'jisho.org/word/'
-
-
-def bot_registered(conn, token: uuid.UUID) -> bool:
-    registered = False
-
-    tx = conn.cursor()
-
-    tx.execute("""select exists(select 1 from bots where token=%s)""", (token,))
-    if tx.fetchone() is not None:
-        registered = True
-
-    tx.close()
-
-    return registered
+JISHO_URL = 'jisho.org/search/'
 
 
-def create(conn, chat_id: str, word: str, provider: str = JISHO_URL) -> int:
-    """Inserts new word in chat with id chat_id, returns id of inserted word"""
-    if chat_id is None or len(chat_id) == 0:
+def _full_link(provider, word):
+    """Return a website link for dictionary provider and word."""
+    return 'http://' + provider + word
+
+
+def create(conn, chat_id: str, word: str, provider: str = JISHO_URL) -> str:
+    """Inserts new word in chat with id chat_id, returns link to inserted word."""
+    if not chat_id:
         raise ValueError('chat_id is empty')
-    if word is None or len(word) == 0:
+    if not word:
         raise ValueError('word is empty')
-    if provider is None or len(provider) == 0:
+    if not provider:
         raise ValueError('provider is empty')
 
-    start_date = datetime.datetime.utcnow().date()
-    end_date = start_date + datetime.timedelta(weeks=1)
+    now = datetime.datetime.utcnow().date()
+    start_date = now - datetime.timedelta(days=now.weekday())  # Start of the current week — Monday.
+    end_date = start_date + datetime.timedelta(days=6)  # End of the current week — Sunday.
 
     tx = conn.cursor()
+    try:
+        tx.execute(r'select id from links where word=%s and chat_id=%s and start_date <= %s and end_date >= %s;', (word, chat_id, start_date, end_date))
+        row = tx.fetchone()
+        if row:
+            return row[0]
 
-    # TODO: check if already exists for current time period.
-    tx.execute("""insert into links (chat_id, word, start_date, end_date, provider, clicks)
-                         values (%s, %s, %s, %s, %s, 0)
-                  returning id;""",
-               (chat_id, word, start_date, end_date, provider))
-    new_id = tx.fetchone()[0]
+        tx.execute("""insert into links (chat_id, word, start_date, end_date, provider, clicks)
+                             values (%s, %s, %s, %s, %s, 0)
+                      returning id;""",
+                   (chat_id, word, start_date, end_date, provider))
+        new_id = tx.fetchone()[0]
 
-    conn.commit()
-    tx.close()
+        conn.commit()
 
-    return new_id
+        return new_id
+    finally:
+        tx.close()
 
 
 # todo: Consider limiting clicks from one user somehow.
@@ -53,42 +49,44 @@ def handle_click(conn, link_id: int) -> str:
     current_date = datetime.datetime.utcnow().date()
 
     tx = conn.cursor()
+    try:
+        tx.execute(r'select word, start_date, end_date, provider, clicks from links where id=%s;', (link_id,))
+        row = tx.fetchone()
+        if not row:
+            raise ValueError('id "' + str(link_id) + '" is not found')
 
-    tx.execute(r'select word, start_date, end_date, provider, clicks from links where id=%s;', (link_id,))
-    row = tx.fetchone()
-    if row is None:
-        raise ValueError('id "' + str(link_id) + '" is not found')
-    word = row[0]
-    start_date = row[1]
-    end_date = row[2]
-    provider = row[3]
-    clicks = row[4]
+        word = row[0]
+        start_date = row[1]
+        end_date = row[2]
+        provider = row[3]
+        clicks = row[4]
 
-    full_link = "http://" + provider + word  # todo: Use provider as template instead.
+        if start_date <= current_date <= end_date:
+            tx.execute("""update links set clicks = %s where id = %s""", (clicks + 1, link_id))
 
-    if start_date <= current_date <= end_date:
-        tx.execute("""update links set clicks = %s where id = %s""",
-                   (clicks + 1, link_id))
+        conn.commit()
 
-    conn.commit()
-    tx.close()
-
-    return full_link
+        return _full_link(provider, word)
+    finally:
+        tx.close()
 
 
-def get_words_with_clicks(conn, chat_id: str, start_date: datetime.date, end_date: datetime.date):
-    """Returns top 5 most clicked words as pairs (word, clicks) for chat with chat_id with matching start and end dates."""
+def get_words_with_clicks(conn, chat_id: str, current_date: datetime.date):
+    """
+    Returns top 5 most clicked words as pairs (word, clicks) for chat with chat_id
+    and with start-end interval that includes current_date.
+    """
     tx = conn.cursor()
+    try:
+        tx.execute("""select word, provider, clicks from links
+                   where chat_id=%s and start_date <= %s and end_date >= %s
+                   order by clicks desc
+                   limit 5;""", (chat_id, current_date, current_date))
 
-    tx.execute("""select word, clicks from links
-               where chat_id=%s and start_date=%s and end_date=%s
-               order by clicks desc
-               limit 5;""", (chat_id, start_date, end_date))
+        words_with_clicks = []
+        for row in tx.fetchall():
+            words_with_clicks.append((_full_link(row[1], row[0]), row[2]))
 
-    words_with_clicks = []
-    for row in tx.fetchall():
-        words_with_clicks.append((row[0], row[1]))
-
-    tx.close()
-
-    return words_with_clicks
+        return words_with_clicks
+    finally:
+        tx.close()
