5732776db2c33e38e05668896ddc77846e5874b9
alois9866 <5354247+alois9866@users.noreply.github.com>
2021-04-10 19:20:03 +0300

    Add server API (#9)
    * Add endpoint for link creation WIP
    
    Also added new table for bot tokens.
    
    * Remove 'bots' table
    
    * Add API description
    
    * Rewrite request processing logic
    
    * Add API description to README

--- server/main.py
+++ server/main.py
@@ -11,26 +11,31 @@ import application
 USER_VAR = 'DBUSER'
 PASSWORD_VAR = 'DBPASSWORD'
 DB_HOST_VAR = 'DBHOST'
+BOT_TOKEN_HASH_VAR = 'BOT_TOKEN_HASH'
 
 HOST = '0.0.0.0'
 PORT = 80
 
 if __name__ == '__main__':
     user = os.environ.get(USER_VAR)
-    if user is None:
+    if not user:
         sys.exit(USER_VAR + ' is not set')
 
     password = os.environ.get(PASSWORD_VAR)
-    if password is None:
+    if not password:
         sys.exit(PASSWORD_VAR + ' is not set')
 
     db_host = os.environ.get(DB_HOST_VAR)
-    if db_host is None:
+    if not db_host:
         sys.exit(DB_HOST_VAR + ' is not set')
+
+    bot_token_hash = os.environ.get(BOT_TOKEN_HASH_VAR)
+    if not bot_token_hash:
+        sys.exit(BOT_TOKEN_HASH_VAR + ' is not set')
 
     conn = psycopg2.connect(database="postgres", user=user, password=password, host=db_host, port="5432")
     try:
-        app = application.initialize_app(conn)
+        app = application.initialize_app(conn, bot_token_hash)
         print(f'Starting server at {HOST}:{PORT}')
         bjoern.run(app, HOST, PORT)
     finally:
